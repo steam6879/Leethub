5d6274478c89d1ee98e50686cb277a893de0ae90
beeen <mhosk25@gmail.com>
2023-12-31 20:31:07 +0900

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,16 @@
+from typing import List
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {}
+    def twoSum(self, nums: List[int], target: int) -> List:
         n = len(nums)
+        nums_table = {}
+        complement = 0
 
         for i in range(n):
             complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
 
-        return []  # No solution found
+            if complement in nums_table:
+                return [nums_table[complement], i]
+            else:
+                nums_table[nums[i]] = i
+        
+        return []
