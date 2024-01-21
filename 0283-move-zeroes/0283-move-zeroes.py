4272c18d50b5310c40b0151dbc1f705a01eccac4
beeen <mhosk25@gmail.com>
2024-01-21 16:46:20 +0900

    Time: 138 ms (62.18%), Space: 18.1 MB (59.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+from typing import List
+# two pointer algorithm
+class Solution:
+     def moveZeroes(self, nums):
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if (nums[j] != 0):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
