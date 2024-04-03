ac72aed186e051a96c76bb04d11a4b2afbafbd40
beeen <mhosk25@gmail.com>
2024-04-03 21:45:14 +0900

    Time: 65 ms (31.44%), Space: 16.7 MB (44.51%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        m = defaultdict(lambda: 0)
+        for char in magazine:
+            m[char] += 1
+
+        for char in ransomNote:
+            if char in m:
+                if m[char] > 0:
+                    m[char] -= 1
+                else:
+                    return False
+            
+            else:
+                return False
+            
+        else:
+            return True
