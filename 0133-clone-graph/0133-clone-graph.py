968a1314310f2c0116efe9261cf1ac6f82e4f318
beeen <mhosk25@gmail.com>
2024-07-21 18:33:03 +0900

    Time: 40 ms (59.79%), Space: 16.8 MB (57.53%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from collections import deque
+
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        clone = Node(node.val)
+        clones = {node: clone}
+        que = deque([node])
+
+        while que:
+            node = que.popleft()
+            for nei in node.neighbors:
+                if nei not in clones:
+                    clones[nei] = Node(nei.val)
+                    que.append(nei)
+                clones[node].neighbors.append(clones[nei])
+
+        return clone
