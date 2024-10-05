bf6d374b4232e0cf996c77317a30c91d46550466
beeen <mhosk25@gmail.com>
2024-10-05 18:42:30 +0900

    Time: 32 ms (84.16%), Space: 16.8 MB (30.34%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,8 @@
-from typing import List
-
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = [[]]
 
-        def dfs(start, visited):
-            ans.append(visited)
-
-            for i in range(start, len(nums)):
-                dfs(i + 1, visited + [nums[i]])
-
-        dfs(0, [])
+        for num in nums:
+            ans += [[num] + item for item in ans]
 
         return ans
