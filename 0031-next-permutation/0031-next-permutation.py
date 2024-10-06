b6e9e8fc2835d4578ec3194efb580b565a27e91a
beeen <mhosk25@gmail.com>
2024-10-06 18:22:23 +0900

    Time: 42 ms (62.77%), Space: 16.6 MB (7.92%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i - 1] < nums[i]:
+                nums[i:] = sorted(nums[i:])
+
+                j = i - 1
+
+
+                for k in range(i, len(nums)):
+                    if nums[j] < nums[k]:
+                        nums[k], nums[j] = nums[j], nums[k]
+                        return nums
+
+        return nums.reverse()
