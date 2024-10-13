df344070e1b195b7a0eaac8c4208b4756ce1e744
beeen <mhosk25@gmail.com>
2024-10-13 16:54:21 +0900

    Time: 43 ms (7.33%), Space: 16.6 MB (51.58%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -7,7 +7,7 @@ class Solution:
         if not matrix:
             return []
 
-        result = []  # This will store the spiral order
+        ans = []  # This will store the spiral order
         left, right = 0, len(matrix[0])  # Initialize the left and right boundaries
         top, bottom = 0, len(matrix)  # Initialize the top and bottom boundaries
 
@@ -15,12 +15,12 @@ class Solution:
         while left < right and top < bottom:
             # Traverse from left to right on the top row
             for i in range(left, right):
-                result.append(matrix[top][i])
+                ans.append(matrix[top][i])
             top += 1  # Move the top boundary down
 
             # Traverse from top to bottom on the right column
             for i in range(top, bottom):
-                result.append(matrix[i][right - 1])
+                ans.append(matrix[i][right - 1])
             right -= 1  # Move the right boundary left
 
             # Check if the boundaries have overlapped
@@ -29,12 +29,12 @@ class Solution:
 
             # Traverse from right to left on the bottom row
             for i in range(right - 1, left - 1, -1):
-                result.append(matrix[bottom - 1][i])
+                ans.append(matrix[bottom - 1][i])
             bottom -= 1  # Move the bottom boundary up
 
             # Traverse from bottom to top on the left column
             for i in range(bottom - 1, top - 1, -1):
-                result.append(matrix[i][left])
+                ans.append(matrix[i][left])
             left += 1  # Move the left boundary right
 
-        return result
+        return ans
