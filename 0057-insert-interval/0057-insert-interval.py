2d54ddc849c0cb57dc01a9ac1b9668bd53d90f1b
beeen <mhosk25@gmail.com>
2024-10-22 23:12:14 +0900

    Time: 0 ms (100%), Space: 18.8 MB (97.02%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,17 +5,18 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
 
-        for i in range(len(intervals)):
-            if intervals[i][1] < newInterval[0]:    # case 1
-                ans.append(intervals[i])
-
-            elif intervals[i][0] > newInterval[1]:  # case 3
+        for interval in intervals:
+            # 현재 interval이 newInterval보다 앞에 있는 경우
+            if interval[1] < newInterval[0]:
+                ans.append(interval)
+            # 현재 interval이 newInterval보다 뒤에 있는 경우
+            elif interval[0] > newInterval[1]:
                 ans.append(newInterval)
 
-                return ans + intervals[i:]
-
-            else:   # case 2
-                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+                return ans + intervals[intervals.index(interval):]
+            # 현재 interval이 newInterval과 겹치는 경우
+            else:
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
 
         ans.append(newInterval)
 
