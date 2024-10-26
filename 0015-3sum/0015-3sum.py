761b7dc54feb76cfc4a10a45e2571b62e6d69720
beeen <mhosk25@gmail.com>
2024-10-26 16:45:45 +0900

    Time: 1265 ms (16.94%), Space: 19.2 MB (99.09%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,24 @@
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
-            return None
-        
-        ans = []
+        triplets = set()
         nums.sort()
 
-        for i, num in enumerate(nums):
-            if i > 0 and num == nums[i - 1]:
-                continue
-            
-            left, right = i + 1, len(nums) - 1
+        for i in range(len(nums) - 2):
+            low, high = i + 1, len(nums) - 1
 
-            while left < right:
-                target = num + nums[left] + nums[right]
-                if target > 0:    right -= 1
-                elif target < 0:    left += 1
+            while low < high:
+                three_sum = nums[i] + nums[low] + nums[high]
+                if three_sum < 0:
+                    low += 1
+                elif three_sum > 0:
+                    high -= 1
 
                 else:
-                    ans.append([num, nums[left], nums[right]])
-                    left += 1
+                    triplets.add((nums[i], nums[low], nums[high]))
+                    low += 1
 
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
+                    while low < high and nums[low] == nums[low - 1]:
+                        low += 1
 
-        return ans
+        return list(triplets)
