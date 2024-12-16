294d8c2138ae23f1fb33cf1b9f26af8430abf00f
beeen <mhosk25@gmail.com>
2024-12-16 20:42:13 +0900

    Time: 1 ms (80.46%), Space: 25 MB (27%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        k %= len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        k = k % len(nums)
+        nums.reverse()
+
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
 
         return nums
