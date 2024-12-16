3349bc39dd8355d065a2192cb4c06598ec977872
beeen <mhosk25@gmail.com>
2024-12-16 21:20:01 +0900

    Time: 100 ms (73.31%), Space: 23 MB (33.98%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,21 +1,21 @@
+from typing import List
+
+
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        if len(nums) <= 1:
-            return 0
+        prefix_sum = 0  # 누적 합 초기화
+        hash_map = {0: -1}  # 누적 합이 처음 나타난 인덱스를 저장하는 해시 맵
+        max_length = 0  # 최대 길이 초기화
 
-        maxLength = 0
-        m = {0: -1}
-        count = 0
+        for i in range(len(nums)):
+            # nums[i]가 0이면 -1을, 1이면 +1을 누적 합에 더함
+            prefix_sum += -1 if nums[i] == 0 else 1
 
-        for i, num in enumerate(nums):
-            if num == 0:
-                count -= 1
+            if prefix_sum in hash_map:
+                # 이전에 같은 누적 합이 있었다면, 그 인덱스부터 현재 인덱스까지의 길이 계산
+                max_length = max(max_length, i - hash_map[prefix_sum])
             else:
-                count += 1
+                # 누적 합이 처음 등장하면 현재 인덱스를 해시 맵에 저장
+                hash_map[prefix_sum] = i
 
-            if count in m:
-                maxLength = max(maxLength, i - m[count])
-            else:
-                m[count] = i
-
-        return maxLength
+        return max_length  # 가장 긴 부분 배열의 길이 반환
