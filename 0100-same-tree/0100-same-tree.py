dcdeb6cedf44a3a0d4f32daf2b4f4f7d3f974eb3
beeen <mhosk25@gmail.com>
2025-01-09 14:43:19 +0900

    Time: 0 ms (100%), Space: 18 MB (7.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,22 +1,29 @@
 # Definition for a binary tree node.
 from typing import Optional
+from collections import deque
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-class Solution:
+
+
+class Solution:     # DFS
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # Base case: both nodes are None
-        if not p and not q:
-            return True
-        # If one node is None and the other is not, they are not the same
-        elif not p or not q:
-            return False
-        # Compare the values of the current nodes
-        elif p.val != q.val:
-            return False
-        # Recursively check the left and right subtrees
-        else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        def dfs(p, q):
+            if not p and not q:
+                return True
+
+            elif not p or not q:
+                return False
+
+            elif p.val != q.val:
+                return False
+            
+            else:
+                return dfs(p.left, q.left) \
+                    and dfs(p.right, q.right)
+
+        return dfs(p, q)
