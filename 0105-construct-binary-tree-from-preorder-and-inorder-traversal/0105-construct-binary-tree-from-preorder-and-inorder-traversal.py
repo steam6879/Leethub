475827abd8b7d104454d5f85429fc4d0634756da
beeen <mhosk25@gmail.com>
2025-01-09 21:58:07 +0900

    Time: 67 ms (61.04%), Space: 53.9 MB (54.2%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,20 +1,19 @@
-from typing import Optional, List
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution:
+class Solution:     # Copilot ver``
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if inorder:
-            i = inorder.index(preorder.pop(0))
-
-            root = TreeNode(inorder[i])
-            root.left = self.buildTree(preorder, inorder[:i])
-            root.right = self.buildTree(preorder, inorder[i+1:])
-
-            return root
+        if not inorder:  # Base case: empty tree
+            return None
+            
+        # Get root value from preorder and find its position in inorder
+        root_val = preorder.pop(0)
+        root_idx = inorder.index(root_val)
+        
+        # Create root node
+        root = TreeNode(root_val)
+        
+        # Build left subtree: elements before root in inorder
+        root.left = self.buildTree(preorder, inorder[:root_idx])
+        
+        # Build right subtree: elements after root in inorder
+        root.right = self.buildTree(preorder, inorder[root_idx + 1:])
+        
+        return root
