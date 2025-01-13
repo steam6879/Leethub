1c9eda58d56fa55cc77106541aae2d121ee33d9a
beeen <mhosk25@gmail.com>
2025-01-13 22:17:16 +0900

    Time: 163 ms (50.45%), Space: 19.7 MB (77.6%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,34 +1,26 @@
 from collections import deque
 
-
 class Solution:
     def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
         rows, cols = len(mat), len(mat[0])
         que = deque()
-
+        
+        # Initialize distances and queue
         for i in range(rows):
             for j in range(cols):
                 if mat[i][j] == 0:
                     que.append((i, j))
-
-        visited = set()
-        visited.update(que)
-        count = 0
-
+                else:
+                    mat[i][j] = float('inf')
+        
+        # BFS
         while que:
-            for _ in range(len(que)):
-                i, j = que.popleft()
-
-                for x, y in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
-                    if (0 <= x < rows and
-                        0 <= y < cols and
-                        (x, y) not in visited):
-                        que.append((x, y))
-                        visited.add((x, y))
-
-                if mat[i][j] != 0:
-                    mat[i][j] += count - 1
-
-            count += 1
-
+            i, j = que.popleft()
+            for x, y in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+                if (0 <= x < rows and
+                    0 <= y < cols and
+                    mat[x][y] > mat[i][j] + 1):
+                    mat[x][y] = mat[i][j] + 1
+                    que.append((x, y))
+                    
         return mat
