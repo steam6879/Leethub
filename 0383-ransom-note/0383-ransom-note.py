5af91de5c659d51ebeffef03b3a558c12f2a2a30
beeen <mhosk25@gmail.com>
2025-02-02 21:26:45 +0900

    Time: 13 ms (81.36%), Space: 17.9 MB (43.34%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,14 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        m = defaultdict(lambda: 0)
-        for char in magazine:
-            m[char] += 1
+        counter = Counter(magazine)
 
         for char in ransomNote:
-            if char in m:
-                if m[char] > 0:
-                    m[char] -= 1
-                else:
-                    return False
-
-            else:
+            if not counter[char]:
                 return False
 
-        else:
-            return True
+            counter[char] -= 1
+
+        return True
