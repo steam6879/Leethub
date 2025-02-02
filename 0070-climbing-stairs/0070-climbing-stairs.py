efeacedcc31140ee7ca4185683e20940fb8da0d7
beeen <mhosk25@gmail.com>
2025-02-02 21:44:35 +0900

    Time: 0 ms (100%), Space: 17.7 MB (38.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.dp = {1: 1, 2: 2}
-
     def climbStairs(self, n: int) -> int:
-        if n not in self.dp:
-            self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        if n == 1:
+            return 1
         
-        return self.dp[n]
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
