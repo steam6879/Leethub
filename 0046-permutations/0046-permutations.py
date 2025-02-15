eb75c4e9e77099dea916814d3dc888858469402d
beeen <mhosk25@gmail.com>
2025-02-15 13:40:32 +0900

    Time: 0 ms (100%), Space: 18.1 MB (14.31%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -7,8 +7,7 @@ class Solution:
 
         def dfs(picked, unpicked):
             if not unpicked:
-                return ans.append(picked)
-
+                ans.append(picked)
             for i, num in enumerate(unpicked):
                 dfs(picked + [num], unpicked[:i] + unpicked[i + 1:])
 
