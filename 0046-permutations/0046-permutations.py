802dcb853dbaf63a7a917046e11196da09a3ca2b
beeen <mhosk25@gmail.com>
2025-02-15 14:01:24 +0900

    Time: 0 ms (100%), Space: 18.2 MB (14.31%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -5,12 +5,20 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
 
-        def dfs(picked, unpicked):
-            if not unpicked:
-                ans.append(picked)
-            for i, num in enumerate(unpicked):
-                dfs(picked + [num], unpicked[:i] + unpicked[i + 1:])
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                ans.append(path[:])
+                return None
+            
+            for i in range(len(nums)):
+                if i not in used:
+                    used.add(i)
+                    path.append(nums[i])
 
-        dfs([], nums)
+                    backtrack(path, used)
 
+                    path.pop()
+                    used.remove(i)
+
+        backtrack([], set())
         return ans
